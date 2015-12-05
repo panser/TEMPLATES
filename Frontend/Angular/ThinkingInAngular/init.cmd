20da06f43042abf1844709531febb496987f1839
panser <gostroy@gmail.com>
2015-12-05 19:28:21 +0200

    SmartMe: generate project with generator-gulp-angular

---
+++ Frontend/Angular/ThinkingInAngular/init.cmd
@@ -0,0 +1,5 @@
+npm install -g yo gulp bower
+npm install -g generator-gulp-angular
+
+mkdir tst4 && cd $_
+yo gulp-angular
---
+++ Frontend/Angular/ThinkingInAngular/tst4/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('tst4')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
